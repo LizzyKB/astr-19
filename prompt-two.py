f92c4d6a3d36074883a930a0a69ab14afb51900c
Liz <146883079+LizzyKB@users.noreply.github.com>
2023-10-20 00:30:28 -0700

    Session 2 prompt of coding journal 1
    >:|

---
+++ prompt-two.py
@@ -0,0 +1,22 @@
+x = float(5.5)
+y = float(34.7)
+
+result = x +y  # Get sum of two floats using + operator
+print("result = ", result)
+
+
+int_num = input('6.9: ')
+float_num = 4.3
+
+product = int(int_num) * float_num
+print('The product of {int_num} and {float_num} is {product}')
+
+
+n1 = int(input("97: "))
+n2 = int(input("-5: "))
+if n1 > n2:
+	diff = 97 - 5
+else:
+	diff = -5 - 97
+
+print("The difference is:",diff)
