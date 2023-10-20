466efcc159a186db842fe62053789e0edab4f947
Liz <146883079+LizzyKB@users.noreply.github.com>
2023-10-19 22:24:35 -0700

    Session 1 prompt for coding journal 1
    :) I DID IT!

---
+++ Prompt-one.py
@@ -0,0 +1,20 @@
+def PrintMynameisElizabethBrooks():
+	print("My name is Elizabeth Brooks!")
+
+def main():
+	PrintMynameisElizabethBrooks()
+
+if __name__=="__main__":
+	main()
+
+def PrintMyfavoritefoodiswalnutshrimpbecauseitssweetMyfavoritemovieisRealSteelitsbasicallyarobotboxingmovie():
+	print("My favorite food is walnut shrimp because its sweet. My favorite movie is Real Steel its basically a robot boxing movie.")
+
+def main():
+	PrintMyfavoritefoodiswalnutshrimpbecauseitssweetMyfavoritemovieisRealSteelitsbasicallyarobotboxingmovie()
+
+if __name__=="__main__":
+	main()
+
+
+
